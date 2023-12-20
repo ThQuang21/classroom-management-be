5b7c6abebbaa3234ffba9a7efd9434bd685612d4
ThQuang21 <thienquang213@gmail.com>
2023-12-20 18:34:50 +0700

    Setup socket to send msg

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser")
 const cookieParser = require("cookie-parser")
 const session = require('express-session')
 const passport = require('passport')
-
+const { Server } = require('socket.io');
 const authRoutes = require('./routes/authRoute');
 const classRoutes = require('./routes/classRoute');
 const gradeRoutes = require('./routes/gradeRoute');
@@ -14,6 +14,7 @@ require("dotenv").config();
 require("./utils/connectDB")
 
 const app = express()
+
 app.use(cors({origin: true, credentials: true}));
 app.use(bodyParser.json())
 app.use(cookieParser())
@@ -27,6 +28,21 @@ app.use(passport.session());
 
 const port = 3000
 
+const http = require("http");
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`User connected ${socket.id}`);
+});
+io.on("identity", (userId) => {
+  console.log(`User connected ${socket.id}`);
+  console.log(`User connected ${userId}`);
+});
 
 app.get('/', (req, res) => {
   res.send('Classroom Management')
@@ -37,6 +53,6 @@ app.use('/classes', classRoutes); // Class routes
 app.use('/grades', gradeRoutes); // Grade routes
 app.use('/gradeReviews', gradeReviews); // Grade review routes
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
